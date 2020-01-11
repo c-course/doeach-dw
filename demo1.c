88e613c47c06c549445531e0107cebeb4ca81ac5
doeach-dw <doeach@qq.com>
2020-01-11 16:13:02 +0800

    File copy
    Resolve #1
    
    Signed-off-by: doeach-dw <doeach@qq.com>

--- demo1.c
+++ demo1.c
@@ -1,4 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 int main()
 {
-	return 1;
+	int fd, fc;
+	char buf;
+
+    fd = open("test.txt", O_RDWR);
+    fc = open("test1.txt", O_RDWR | O_CREAT, 0600);
+	if (fd <= 0) {
+		exit(1);
+	}
+	while (read(fd, &buf, 1) > 0) {
+		write(fc, &buf, 1);
+    }
+    close(fd);
+    close(fc);
+    return 0;
 }
